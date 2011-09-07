1c670af851d503b471b4f18e082f1b3dd7eaf90a
rmgarnett <romanempire@gmail.com>
2011-09-07 11:12:13 -0400

    deleting not ready covariances

--- cov/covSEiso.c
+++ cov/covSEiso.c
@@ -6,36 +6,40 @@
 void mexFunction(int nlhs, mxArray *plhs[],
 								 int nrhs, const mxArray *prhs[])
 {
+	double *hyperparameters, *in, *out, *training_points, *testing_points, 
+		*self_covariances, *cross_covariances, *hyperparameter;
+
+	int num_points, num_training_points, num_testing_points, dim, i, j, k;
+	double input_scale, output_scale;
+	double squared_distance = 0, difference;
+
+	char *string;
+
 	/* number of hyperparameters */
   if ((nlhs <= 1) && (nrhs == 0)) {
 		plhs[0] = mxCreateString("2");
 		return;
 	}
 
+	/* minimal error checking */
+	if ((mxGetN(prhs[0]) * mxGetM(prhs[0])) != 2) {
+		mexErrMsgTxt("wrong number of hyperparameters!");
+		return;
+	}
+
+	/* grab hyperparamters, first is log(input_scale), second is
+		 log(output_scale) */
+	hyperparameters = mxGetPr(prhs[0]);
+	input_scale = exp(hyperparameters[0]);
+	output_scale = exp(2 * hyperparameters[1]);
+
 	/* training covariance */
-	else if (nrhs == 2) {
-		double *hyperparameters, *in, *out;
-
-		int num_points, dim, i, j, k;
-		double input_scale, output_scale;
-		double squared_distance = 0, difference;
-
-		/* minimal error checking */
-		if (mxGetM(prhs[0]) != 2 || mxGetN(prhs[0]) != 1) {
-			mexErrMsgTxt("wrong number of hyperparameters!");
-			return;
-		}
+	if (nrhs == 2) {
 
 		num_points = mxGetM(prhs[1]);
 		dim = mxGetN(prhs[1]);
 		in = mxGetPr(prhs[1]);
 
-		/* grab hyperparamters, first is log(input_scale), second is
-			 log(output_scale) */
-		hyperparameters = mxGetPr(prhs[0]);
-		input_scale = exp(hyperparameters[0]);
-		output_scale = exp(2 * hyperparameters[1]);
-		
 		plhs[0] = mxCreateDoubleMatrix(num_points, num_points, mxREAL);
 		out = mxGetPr(plhs[0]);
 		
@@ -61,30 +65,9 @@ void mexFunction(int nlhs, mxArray *plhs[],
 	/* test covariances */
 	else if (nrhs == 3) {
 
-		double *hyperparameters, *training_points, *testing_points, 
-			*self_covariances, *cross_covariances;
-
-		int num_training_points, num_testing_points, dim, i, j, k;
-		double input_scale, output_scale;
-		double squared_distance = 0, difference;
-
-		char *string;
-
-		/* minimal error checking */
-		if (mxGetM(prhs[0]) != 2 || mxGetN(prhs[0]) != 1) {
-			mexErrMsgTxt("wrong number of hyperparameters!");
-			return;
-		}
-
 		num_training_points = mxGetM(prhs[1]);
 		dim = mxGetN(prhs[1]);
 		training_points = mxGetPr(prhs[1]);
-
-		/* grab hyperparamters, first is log(input_scale), second is
-			 log(output_scale) */
-		hyperparameters = mxGetPr(prhs[0]);
-		input_scale = exp(hyperparameters[0]);
-		output_scale = exp(2 * hyperparameters[1]);
 
 		if (mxIsChar(prhs[2])) {
 			string = mxArrayToString(prhs[2]);
@@ -99,6 +82,7 @@ void mexFunction(int nlhs, mxArray *plhs[],
 			}	 
 		}
 		else {
+
 			num_testing_points = mxGetM(prhs[2]);
 			testing_points = mxGetPr(prhs[2]);
 			
@@ -134,27 +118,10 @@ void mexFunction(int nlhs, mxArray *plhs[],
 
 	/* derivatives with respect to hyperparamters */
 	else {
-		double *hyperparameter, *hyperparameters, *in, *out;
-
-		int num_points, dim, i, j, k;
-		double input_scale, output_scale;
-		double squared_distance = 0, difference;
 
 		num_points = mxGetM(prhs[1]);
 		dim = mxGetN(prhs[1]);
 		in = mxGetPr(prhs[1]);
-
-		/* minimal error checking */
-		if (mxGetM(prhs[0]) != 2 || mxGetN(prhs[0]) != 1) {
-			mexErrMsgTxt("wrong number of hyperparameters!");
-			return;
-		}
-
-		/* grab hyperparamters, first is log(input_scale), second is
-			 log(output_scale) */
-		hyperparameters = mxGetPr(prhs[0]);
-		input_scale = exp(hyperparameters[0]);
-		output_scale = exp(2 * hyperparameters[1]);
 
 		hyperparameter = mxGetPr(prhs[3]);
 		
