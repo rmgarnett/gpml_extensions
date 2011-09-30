1d4af8b47977c18c65f5514f56da77824df449d7
rmgarnett <romanempire@gmail.com>
2011-09-29 20:55:30 -0400

    formatting and splitting off separate estimate_latent_posterior for fixed discrete case

--- marginalization/calculate_hypersample_weights.m
+++ marginalization/calculate_hypersample_weights.m
@@ -1,16 +1,22 @@
 function hypersample_weights = calculate_hypersample_weights(hypersamples)
 
+  hypersamples.log_likelihoods(isinf(hypersamples.log_likelihoods)) ...
+      = min(hypersamples.log_likelihoods(isfinite(hypersamples.log_likelihoods))) - 1e3;
+
   [quad_noise_sd, quad_input_scales, quad_output_scale] = ...
-      hp_heuristics(hypersamples.values, hypersamples.log_likelihoods, 100);
+      hp_heuristics(hypersamples.values(:, hypersamples.marginal_ind), ...
+                    hypersamples.log_likelihoods, 100);
 
   quad_gp.quad_noise_sd = quad_noise_sd;
   quad_gp.quad_input_scales = quad_input_scales;
   quad_gp.quad_output_scale = quad_output_scale;
   
-  [num_hypersamples, num_hyperparameters] = size(hypersamples.values);
+  num_hypersamples = size(hypersamples.values, 1);
+  num_hyperparameters = length(hypersamples.marginal_ind);
   
   for i = 1:num_hypersamples
-    gp.hypersamples(i).hyperparameters = hypersamples.values(i, :);
+    gp.hypersamples(i).hyperparameters = ...
+        hypersamples.values(i, hypersamples.marginal_ind);
   end
   
   for i = 1:num_hyperparameters
