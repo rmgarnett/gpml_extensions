8b9ad2f6f31d87bab48ec1fa237df50335bf480e
rmgarnett <romanempire@gmail.com>
2011-11-07 22:29:40 -0500

    made generic

--- tests/test_gp_likelihood.m
+++ tests/test_gp_likelihood.m
@@ -1,54 +1,39 @@
-number_of_runs = 5;
-
-sizes = [10 20 50 100 200 500 1000 2000 5000];
-
-for size = sizes
-  data = linspace(0, 100, size)';
-  responses = sin(data(:));
-
-  inference_method = @infExact;
-  mean_function = @meanZero;
-  covariance_function = @covSEiso;
-  likelihood = @likGauss;
-
-  hyperparameters.lik = log(1e-2);
-  hyperparameters.cov = [log(pi); log(1)];
-
-  [hyperparameters, inference_method, mean_function, ...
-   covariance_function, likelihood] = ...
-      check_gp_arguments(hyperparameters, inference_method, ...
-                         mean_function, covariance_function, likelihood, ...
-                         data, responses);
-
-  tic;
-  for i = 1:number_of_runs
-    [nlZ dnlZ post] = gp_likelihood(hyperparameters, inference_method, ...
-                                    mean_function, covariance_function, ...
-                                    likelihood, data, responses);
-  end
-  elapsed = toc;
-
-  disp('gp_likelihood answer: ');
-  disp(['               log likelihood: ' num2str(-nlZ)]);
-  for i = 1:length(dnlZ.cov)
-    disp(['  log likelihood derivative ' num2str(i) ': ' num2str(dnlZ.cov(i))]);
-  end
-  disp(['                   total time: ' num2str(elapsed) 's.']);
-  disp(['                 average time: ' num2str(elapsed / number_of_runs) 's.']);
+function passed = test_gp_likelihood(hyperparameters, inference_method, ...
+        mean_function, covariance_function, likelihood, data, ...
+        responses, number_of_runs)
 
   tic
   for i = 1:number_of_runs
-    [nlZ dnlZ] = gp(hyperparameters, inference_method, ...
-                    mean_function, covariance_function, ...
-                    likelihood, data, responses);
+    [gpml_nlZ gpml_dnlZ] = gp(hyperparameters, inference_method, mean_function, ...
+                              covariance_function, likelihood, data, responses);
   end
-  elapsed = toc;
+  gpml_elapsed = toc;
+
+  tic;
+  for i = 1:number_of_runs
+    [gp_likelihood_nlZ gp_likelihood_dnlZ] = gp_likelihood(hyperparameters, ...
+            inference_method, mean_function, covariance_function, ...
+            likelihood, data, responses);
+  end
+  gp_likelihood_elapsed = toc;
+
+  passed = (gpml_nlZ == gp_likelihood_nlZ) && ...
+           all(unwrap(gpml_dnlZ) == unwrap(gp_likelihood_dnlZ));
 
   disp('gp answer: ');
-  disp(['               log likelihood: ' num2str(-nlZ)]);
-  for i = 1:length(dnlZ.cov)
-    disp(['  log likelihood derivative ' num2str(i) ': ' num2str(dnlZ.cov(i))]);
+  disp(['               log likelihood: ' num2str(-gpml_nlZ)]);
+  for i = 1:length(gpml_dnlZ.cov)
+    disp( ['  log likelihood derivative ' num2str(i) ': ' num2str(gpml_dnlZ.cov(i))]);
   end
-  disp(['                   total time: ' num2str(elapsed) 's.']);
-  disp(['                 average time: ' num2str(elapsed / number_of_runs) 's.']);
+  disp(['                   total time: ' num2str(gpml_elapsed) 's.']);
+  disp(['                 average time: ' num2str(gpml_elapsed / number_of_runs) 's.']);
+
+  disp('gp_likelihood answer: ');
+  disp(['               log likelihood: ' num2str(-gp_likelihood_nlZ)]);
+  for i = 1:length(gp_likelihood_dnlZ.cov)
+    disp( ['  log likelihood derivative ' num2str(i) ': ' num2str(gp_likelihood_dnlZ.cov(i))]);
+  end
+  disp(['                   total time: ' num2str(gp_likelihood_elapsed) 's.']);
+  disp(['                 average time: ' num2str(gp_likelihood_elapsed / number_of_runs) 's.']);
+
 end
