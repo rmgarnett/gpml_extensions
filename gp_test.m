5fb6efcd39d9b5d535de6e03e9935d2f49dffa42
rmgarnett <romanempire@gmail.com>
2011-11-07 22:11:36 -0500

    some rewriting and merging full covariance code into single function

--- gp_test.m
+++ gp_test.m
@@ -5,8 +5,8 @@ function [varargout] = gp_test(hyp, inf, mean, cov, lik, x, y, xs, ys)
 % partial derivatives with respect to the hyperparameters may be
 % returned as well.
 %
-% If the full posterior covariance is desired, please call
-% gp_full_covariance.
+% If the full posterior covariance is desired, you may set:
+%   hyp.full_covariance = true
 %
 % Please call check_gp_arguments before calling this function.
 %
@@ -46,14 +46,17 @@ function [varargout] = gp_test(hyp, inf, mean, cov, lik, x, y, xs, ys)
 % Copyright (c) 2011 Roman Garnett. All rights reserved.
 
 % call the inference method
-nlZ = [];
+ nlZ = [];
 dnlZ = [];
 try
-  if (nargout < 6)        % no likelihood desired
-    post = inf(hyp, mean, cov, lik, x, y);
-  elseif (nargout < 7)    % likelihood desired
-    [post nlZ] = inf(hyp, mean, cov, lik, x, y);
-  else                    % likelihood and derivatives desired
+  % no likelihood desired
+  if (nargout < 6) 
+               post = inf(hyp, mean, cov, lik, x, y);
+  % likelihood desired
+  elseif (nargout < 7) 
+         [post nlZ] = inf(hyp, mean, cov, lik, x, y);
+  % likelihood and derivatives desired
+  else
     [post nlZ dnlZ] = inf(hyp, mean, cov, lik, x, y);
   end
 catch msgstr
@@ -62,8 +65,8 @@ catch msgstr
 end
 
 alpha = post.alpha;
-L = post.L; 
-sW = post.sW;
+   sW = post.sW;
+    L = post.L; 
 
 % handle things for sparse representations
 if (issparse(alpha))
@@ -94,58 +97,97 @@ Ltril = all(all(tril(L, -1) == 0));
 % number of data points
 ns = size(xs, 1);
 
-% number of data points per mini batch
-nperbatch = 5000;
-% number of already processed test data points
-nact = 0;
-
-% allocate outputs
-ymu = zeros(ns, 1); 
-ys2 = zeros(ns, 1);
-fmu = zeros(ns, 1);
-fs2 = zeros(ns, 1);
-lp = zeros(ns, 1);
-
-% process minibatches of test cases to save memory
-while (nact < ns)
-  % data points to process
-  id = (nact + 1):min(nact + nperbatch, ns);
+if (isfield(hyp, 'full_covariance') && hyp.full_covariance)
+  % prior mean
+  ms = feval(mean{:}, hyp.mean, xs);
 
   % cross-covariances
-  Ks  = feval(cov{:}, hyp.cov, x(nz, :), xs(id, :));
-  % self-variance
-  kss = feval(cov{:}, hyp.cov, xs(id, :), 'diag');
-  
-  % prior mean
-  ms = feval(mean{:}, hyp.mean, xs(id, :));
+  Ks = feval(cov{:}, hyp.cov, x, xs);
+
+  % in case L is not provided, we compute it
+  if (numel(L) == 0)
+    %prior covariance
+    K = feval(cov{:}, hyp.cov, x);
+    L = chol(eye(ns) + sW * sW' .* K);
+  end
 
   % predictive means
-  fmu(id) = ms + Ks' * full(alpha(nz));
+  fmu = ms + Ks' * alpha;
 
-  % predictive variances
-  if (Ltril)
-    % L is triangular => use Cholesky parameters (alpha, sW, L)
-    V  = L' \ (repmat(sW, 1, length(id)) .* Ks);
-    if (nargout > 2)
-      % predictive variances
-      fs2(id) = kss - sum(V .* V, 1)';
-    end
-  else
-    % L is not triangular => use alternative parametrisation
-    fs2(id) = kss + sum(Ks .* (L * Ks), 1)';
-  end
-  % remove numerical noise i.e. negative variances
-  fs2(id) = max(fs2(id), 0);
-  
+  % prior test covariance
+  Kss = feval(cov{:}, hyp.cov, xs, xs);
+
+  % predictive covariance
+  V  = L' \ (repmat(sW, 1, size(xs, 1)) .* Ks);
+  fs2 = Kss - V' * V;
+
+  % assign output arguments
   if (nargin < 9)
-    [lp(id) ymu(id) ys2(id)] = lik(hyp.lik, [], fmu(id), fs2(id));
+    [ ~, ymu, ys2] = lik(hyp.lik, [], fmu, diag(fs2));
+    varargout = {ymu, ys2, fmu, fs2, [], nlZ, dnlZ};
   else
-    [lp(id) ymu(id) ys2(id)] = lik(hyp.lik, ys(id), fmu(id), fs2(id));
+    [lp, ymu, ys2] = lik(hyp.lik, ys, fmu, diag(fs2));
+    varargout = {ymu, ys2, fmu, fs2, lp, nlZ, dnlZ};
+  end
+
+  return;
+else
+
+  % allocate outputs
+  ymu = zeros(ns, 1); 
+  ys2 = zeros(ns, 1);
+  fmu = zeros(ns, 1);
+  fs2 = zeros(ns, 1);
+   lp = zeros(ns, 1);
+  
+  % number of data points per mini batch
+  nperbatch = 5000;
+  % number of already processed test data points
+  nact = 0;
+  
+  % process minibatches of test cases to save memory
+  while (nact < ns)
+    % data points to process
+    id = (nact + 1):min(nact + nperbatch, ns);
+    
+    % cross-covariances
+    Ks  = feval(cov{:}, hyp.cov,  x(nz, :), xs(id, :));
+    % self-variance
+    kss = feval(cov{:}, hyp.cov, xs(id, :), 'diag');
+    
+    % prior mean
+    ms = feval(mean{:}, hyp.mean, xs(id, :));
+    
+    % predictive means
+    fmu(id) = ms + Ks' * full(alpha(nz));
+    
+    % predictive variances
+    if (Ltril)
+      % L is triangular => use Cholesky parameters (alpha, sW, L)
+      V  = L' \ (repmat(sW, 1, length(id)) .* Ks);
+      if (nargout > 2)
+        % predictive variances
+        fs2(id) = kss - sum(V .* V, 1)';
+      end
+    else
+      % L is not triangular => use alternative parametrisation
+      fs2(id) = kss + sum(Ks .* (L * Ks), 1)';
+    end
+    % remove numerical noise i.e. negative variances
+    fs2(id) = max(fs2(id), 0);
+    
+    if (nargin < 9)
+      [lp(id) ymu(id) ys2(id)] = lik(hyp.lik,     [], fmu(id), fs2(id));
+    else
+      [lp(id) ymu(id) ys2(id)] = lik(hyp.lik, ys(id), fmu(id), fs2(id));
+    end
+    
+    % set counter to index of last processed data point  
+    nact = id(end);
   end
   
-  % set counter to index of last processed data point  
-  nact = id(end);
+  % assign output arguments
+  varargout = {ymu, ys2, fmu, fs2, lp, nlZ, dnlZ};
+  return;
 end
 
-% assign output arguments
-varargout = {ymu, ys2, fmu, fs2, lp, nlZ, dnlZ};
