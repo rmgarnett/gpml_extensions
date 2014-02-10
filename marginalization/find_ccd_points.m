20e8d58bc75ac7cf0254750a245847a946cd4d1f
Roman Garnett <romanempire@gmail.com>
2014-02-10 14:22:44 +0100

    cleaning up old stuff

--- marginalization/find_ccd_points.m
+++
@@ -1,9 +0,0 @@
-function samples = find_ccd_points(prior_means, prior_variances)
-
-  dimension = length(prior_means);
-
-  samples = ccdesign(dimension, 'center', 1);
-  samples = samples .* repmat(sqrt(prior_variances(:)'), size(samples, 1), 1);
-  samples = samples  + repmat(prior_means(:)', size(samples, 1), 1);
-
-end
