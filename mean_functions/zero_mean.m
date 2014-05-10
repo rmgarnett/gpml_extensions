72fcba2165749463712e8e26abd92e0415e4cba7
rmgarnett <romanempire@gmail.com>
2014-05-10 18:25:39 +0200

    initial import

---
+++ mean_functions/zero_mean.m
@@ -0,0 +1,39 @@
+% ZERO_MEAN zero mean function.
+%
+% This provides a GPML-compatible mean function implementing a
+% zero mean function:
+%
+%   \mu(x) = 0.
+%
+% This can be used as a drop-in replacement for meanZero.
+%
+% This implementation supports an extended GPML syntax that allows
+% calculating the Hessian of \mu with respect to any pair of
+% hyperparameters. The syntax is:
+%
+%   dK2_didj = zero_mean(hyperparameters, x, z, i, j);
+%
+% where dK2_didj is \partial^2 \mu / \partial \theta_i \partial \theta_j,
+% and the Hessian is evaluated at x.
+%
+% These Hessians can be used to ultimately compute the Hessian of the
+% GP training likelihood (see, for example, exact_inference.m).
+%
+% No hyperparameters are required.
+%
+% See also MEANFUNCTIONS.
+
+% Copyright (c) Roman Garnett, 2014
+
+function result = zero_mean(~, x, ~, ~)
+
+  % report number of hyperparameters
+  if (nargin <= 1)
+    result = '0';
+    return;
+  end
+
+  % any other mode always returns the all-zeros matrix.
+  result = zeros(size(x));
+
+end
