d1e47645be8a4efc04ffe214162ef445bb05f922
rmgarnett <romanempire@gmail.com>
2014-05-23 14:29:46 +0200

    initial import

---
+++ inference_methods/add_prior_to_inference_method.m
@@ -0,0 +1,41 @@
+% ADD_PRIOR_TO_INFERNCE_METHOD adds hyperprior to GPML infernce method.
+%
+% This function may be used to generate a GPML compatible inference
+% method by combining a hyperparameter prior p(\theta) with an
+% existing inference method. See inference_with_prior.m for more
+% information.
+%
+% This function is required because GPML does not allow
+% constructing "meta" inference methods in the same way as, e.g.,
+% mean and covariance functions.
+%
+% Usage
+% -----
+%
+%   inference_method = ...
+%       add_prior_to_inference_method(inference_method, prior)
+%
+% Inputs:
+%
+%   inference_method: a GPML compatible inference method
+%              prior: a hyperparameter prior (see priors.m)
+%
+% Outputs:
+%
+%   inference_method: a GPML compatible inference method
+%                     incorporating the given hyperparameter prior
+%
+% See also INFERENCE_WITH_PRIOR, PRIORS.
+
+% Copyright (c) 2014 Roman Garnett.
+
+function inference_method = add_prior_to_inference_method(inference_method, ...
+          prior)
+
+  inference_method = @(hyperparameters, mean_function, covariance_function, ...
+                       likelihood, x, y) ...
+      inference_with_prior(inference_method, prior, hyperparameters, ...
+                           mean_function, covariance_function, ...
+                           likelihood, x, y);
+
+end
